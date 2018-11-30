199f16fd2b0f4cc6a026b3069fea2d538b97ec5c
Yangyang Xia <Yangyang.Xia@ygomi.com>
2018-11-30 18:35:29 +0800

    issues#3 insert syntax transfer update

--- .demo/V13.2.6__insert-syntax.sql
+++ .demo/V13.2.6__insert-syntax.sql
@@ -1,10 +1,12 @@
-create database insert_syntax;
-
 create table insert_syntax_demo (
   id int unsigned not null primary key auto_increment,
   user_name varchar(255) not null,
   sex tinyint not null default 0,
+  age int unsigned not null,
+  bornTime datetime not null default now(),
+  deadTime datetime null, 
 
   unique key `UK_insert_syntax_demo_user_name` (`user_name`(32) asc)
 ) engine=InnoDB;
 
+-- 列名和列值都为空
